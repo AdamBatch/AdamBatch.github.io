a77992b99f0b183a8c3fe2737fe96b2b9709a226
AdamBatch <adam.batchellor@gmail.com>
2025-03-25 18:15:41 -0600

    Built publications page and added resume

--- Projects/JS_files/htem.js
+++
@@ -1,6 +0,0 @@
-function MyComponent() {
-    return React.createElement('h1', null, 'Hello, React!');
-}
-const domContainer = document.querySelector('#my-react-component');
-ReactDOM.render(React.createElement(MyComponent), domContainer);
-
